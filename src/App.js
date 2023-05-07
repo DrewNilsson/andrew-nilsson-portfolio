e2e93af8db8a454b8afe35f218e816d7dc730e93
DrewNilsson <nilsson1322@gmail.com>
2023-05-06 21:43:34 -0700

    Initial setup of Split Panes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import SplitPane, { Pane } from 'split-pane-react';
+import 'split-pane-react/esm/themes/default.css';
 
 function App() {
+  const [sizes, setSizes] = useState(['50%', 'auto']);
+
+  const layoutCSS = {
+    height: '100%',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center'
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ height: '100vh' }}>
+      <SplitPane
+        split='vertical'
+        sizes={sizes}
+        onChange={setSizes}
+      >
+        <Pane minSize={'20%'} maxSize='60%'>
+          <div style={{ ...layoutCSS, background: '#ddd' }}>
+            pane1
+          </div>
+        </Pane>
+        <div style={{ ...layoutCSS, background: '#d5d7d9' }}>
+          pane2
+        </div>
+      </SplitPane>
     </div>
   );
 }
